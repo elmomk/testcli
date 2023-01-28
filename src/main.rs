1a205494d66db45c6f33086514e943389f4d6d77
Salomo Bartholomees <salomob@gmail.com>
2023-01-28 15:33:29 +0100

    testing dotenvy

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,59 @@
-use std::process::Command;
 use clap::Parser;
-use std::process::Child;
+use dirs::home_dir;
+use dotenvy::dotenv;
+use std::env;
+use std::process::Command;
 
 #[derive(Parser)]
 struct Cli {
-    /// 
+    ///
     mode: String,
-    mode2: String,
-    // #[arg(short, long)]
+    #[arg(short, long, default_value = "")]
+    path: String,
     // update: bool,
 }
 
-fn tf_simple(mode: String) -> Child {
-    Command::new("echo")
-            .arg(mode)
-            .arg(mode2)
-            .spawn()
-            .expect("terraform  failed")
+fn ls_path(path: String) {
+    println!("ls path: {}", path);
+    // println!("home: {}", home);
+    Command::new("ls")
+        .current_dir(path)
+        // .arg(path)
+        // .arg(mode2)
+        .spawn()
+        .expect("terraform  failed");
 }
 
 fn main() {
     let args = Cli::parse();
+    let my_home = home_dir().unwrap().to_string_lossy().to_string() + "/.runcli/config";
+    // println!("my home is: {}", &my_home);
+    // try to load environment vairables from three locations
+    // load environment variables from .runrsenv
+    // load environment variables from scripts/.runrsenv
+    // load environment variables from $HOME/.runcli/config
+    // load files in order of precedence
+    // if .runrsenv exists, load it first and ignore the rest
+    // if .runrsenv does not exist, load scripts/.runrsenv
+    // if scripts/.runrsenv does not exist, load append my_home with ".runcli/config"
+    // if $HOME/.runcli/config does not exist, throw an error
+    dotenvy::from_filename(".runrsenv").ok();
+    dotenvy::from_filename("scripts/.runrsenv").ok();
+    dotenvy::from_filename(my_home).ok();
+    dotenv().ok();
+    // println!("print: {}", print.unwrap().to_string_lossy().to_string());
+    let project_path = if args.path == "" {
+        env::var("project_path").unwrap()
+    } else {
+        args.path
+    };
+    // .expect("PROJECT_PATH must be set");
+    println!("project path is: {}", project_path);
+    // print environment variables
+    // println!("env: {:?}", std::env::vars().collect::<Vec<_>>());
 
     println!("tf mode is: {}", args.mode);
-    // println!("update : {}", args.update);
+    println!("path: {}", project_path);
 
-    tf_simple(args.mode);
+    ls_path(project_path.to_string())
 }
